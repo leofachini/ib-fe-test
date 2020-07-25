0657f3403ed147f0aff00d834b5f8bc8f365a908
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-07-25 12:41:17 +0100

    Added GlobalStyles

---
+++ src/styles/variables.ts
@@ -0,0 +1,37 @@
+// https://github.com/felipefialho/felipefialho.com/blob/main/src/styles/variables.js
+// GNU General Public License v3.0
+//
+// ==================================================
+// Variables
+//
+// 1. Colors
+// 2. Misc
+// ==================================================
+
+// --------------------------------------------------
+// 1. Colors
+// --------------------------------------------------
+
+export const Color = {
+  white: `rgba(255, 255, 255, .9)`,
+  black: `rgba(15, 15, 15, .95)`,
+  whiteSolid: `#ffffff`,
+  blackSolid: `#000000`,
+  blue: `#187888`,
+  blueLight: `#a1ecf7`,
+  grayDarker: `#1d1d1d`,
+  grayDark: `#2d2d2d`,
+  gray: `#757575`,
+  grayLight: `#aaaaaa`,
+  grayLighter: `#efefef`,
+  grayWhite: `#f1f1f1`,
+  yellow: `#e6af05`,
+};
+
+// --------------------------------------------------
+// 2. Misc
+// --------------------------------------------------
+
+export const Transition = {
+  default: `.25s`,
+};
