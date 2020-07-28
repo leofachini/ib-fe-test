f95e659b2fd9c614981ea9a4cc2cfd35b2b8f0ae
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-07-28 11:53:09 +0100

    Created store for WeatherForecast

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,9 +7,16 @@ import {
   LocationState
 } from './locations';
 
+import { 
+  weatherForecastReducer,
+  weatherForecastSaga,
+  WeatherForecastState
+} from './weatherForecast';
+
 // The top-level state object
 export interface ApplicationState {
-  locations: LocationState
+  locations: LocationState,
+  weatherForecast: WeatherForecastState
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property
@@ -18,11 +25,12 @@ export interface ApplicationState {
 export const createRootReducer = () =>
   combineReducers({
     locations: locationsReducer,
+    weatherForecast: weatherForecastReducer
   });
 
 // Here we use `redux-saga` to trigger actions asynchronously. `redux-saga` uses something called a
 // "generator function", which you can read about here:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
 export function* rootSaga() {
-  yield all([fork(locationsSaga)]);
+  yield all([fork(locationsSaga), fork(weatherForecastSaga)]);
 }
